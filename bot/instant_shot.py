39e334cb22a78ab064bde644a4c9f287182cbdb0
Павел Морозов <filimon.doors@gmail.com>
2022-07-29 10:45:32 +0300

    add dockstring

--- bot/instant_shot.py
+++ bot/instant_shot.py
@@ -1,5 +1,6 @@
 import logging
 import os
+import sys
 from time import strftime, time
 
 import requests
@@ -15,22 +16,51 @@ MEDIA = os.path.join(
 )
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 THUMBNAIL_TOKEN = os.getenv('THUMBNAIL_TOKEN')
-THUMBNAIL_URL = 'https://api.thumbnail.ws/api/'
-DB_API_URL = 'http://db-api:8000/api/'
+THUMBNAIL_URL = os.getenv('THUMBNAIL_URL')
+DB_API_URL = os.getenv('DB_API_URL')
+
+logging_status = {
+    'error': 'При получении скриншота возникла ошибка.',
+    'save_error': 'При сохранении данных возникла ошибка.',
+    'save_success': 'Данные сохранены успешно.',
+    'success': 'Скриншот получен.'
+}
 
 
-logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-    level=logging.INFO
-)
+def save_data(data) -> None:
+    """Сохраняет данные запроса в БД."""
+    try:
+        requests.post(url=f'{DB_API_URL}request/', json=data)
+    except Exception as error:
+        err_message = logging_status['save_error'], error
+        raise Exception(err_message)
+    else:
+        logging.info(logging_status['save_success'])
 
 
-def get_url(requested_website):
+def save_bot_user(data) -> None:
+    """Сохраняет данные пользователя в БД."""
+    try:
+        requests.post(url=f'{DB_API_URL}user/', json=data)
+    except Exception as error:
+        err_message = logging_status['save_error'], error
+        raise Exception(err_message)
+    else:
+        logging.info(logging_status['save_success'])
+
+
+def get_url(requested_website) -> str:
+    """Возвращает URL запрашиваемого скриншота."""
     return (f'{THUMBNAIL_URL}{THUMBNAIL_TOKEN}/thumbnail/get?'
             f'url=https://{requested_website}&width=640')
 
 
-def get_screenshot(update):
+def get_screenshot(update) -> tuple:
+    """
+    Сохраняет скриншот на сервере и возвращает кортеж, содержащий следующие
+    данные: время, затраченное на выполнение запроса, URL сайта из запроса
+    пользователя, URL запрашиваемого скриншота.
+    """
     request_time = time()
     current_date = strftime("%m%d%y")
     if update.message:
@@ -47,21 +77,27 @@ def get_screenshot(update):
         return time_used, requested_website, url
 
 
-def shot(update: Update, context: CallbackContext):
+def answer(update: Update, context: CallbackContext) -> None:
+    """
+    В ответ на запрос пользователя присылает сообщение-заглушку, которую
+    редактирует после получения скриншота.
+    """
     with requests.get(url=f'{DB_API_URL}message/wait') as r:
         msg = context.bot.send_photo(
             chat_id=update.effective_chat.id,
             caption=r.json().get('text'),
             photo=open(f'{MEDIA}waiting.jpg', 'rb')
         )
-    time_used, requested_website, url = get_screenshot(update)
-    bot_user_id = update.message.from_user.id
-    requested_url = update.message.text
-    data = {'bot_user_id': bot_user_id, 'requested_url': requested_url}
-    requests.post(
-        url=f'{DB_API_URL}request/',
-        json=data
-    )
+    try:
+        time_used, requested_website, url = get_screenshot(update)
+    except Exception as error:
+        err_message = logging_status['error'], error
+        raise Exception(err_message)
+    else:
+        logging.info(logging_status['success'])
+    data = {'bot_user_id': update.message.from_user.id,
+            'requested_url': update.message.text}
+    save_data(data)
     with requests.get(url=f'{DB_API_URL}message/update') as r:
         context.bot.edit_message_media(
             chat_id=msg.chat.id,
@@ -73,7 +109,7 @@ def shot(update: Update, context: CallbackContext):
         )
 
 
-def inline_shot(update: Update, context: CallbackContext):
+def inline_answer(update: Update, context: CallbackContext) -> None:
     query = update.inline_query.query
     if not query:
         return
@@ -90,26 +126,48 @@ def inline_shot(update: Update, context: CallbackContext):
     context.bot.answer_inline_query(update.inline_query.id, results)
 
 
-def start(update: Update, context: CallbackContext):
+def start(update: Update, context: CallbackContext) -> None:
+    """
+    По команде /start присылает сообщение с описанием функционала бота.
+    """
     bot_user = update.message.from_user.to_dict()
-    requests.post(url=f'{DB_API_URL}user/', json=bot_user)
+    save_bot_user(bot_user)
     with requests.get(url=f'{DB_API_URL}message/start') as r:
         context.bot.send_message(chat_id=update.effective_chat.id,
                                  text=r.json().get('text'))
 
 
-def unknown(update: Update, context: CallbackContext):
+def unknown(update: Update, context: CallbackContext) -> None:
+    """
+    В ответ на запрос, не проходящий валидацию, высылает сообщение об ошибке.
+    """
     with requests.get(url=f'{DB_API_URL}message/unknown') as r:
         context.bot.send_message(chat_id=update.effective_chat.id,
                                  text=r.json().get('text'))
 
 
+def check_tokens() -> bool:
+    """Проверка доступности переменных окружения."""
+    return all([THUMBNAIL_TOKEN, THUMBNAIL_URL, TELEGRAM_TOKEN, DB_API_URL])
+
+
 if __name__ == '__main__':
+    logging.basicConfig(
+        level=logging.INFO,
+        format='%(asctime)s, %(levelname)s, %(message)s, %(funcName)s, '
+               '%(lineno)s '
+    )
+    if not check_tokens():
+        logging.critical('Отсутствует обязательная переменная окружения')
+        sys.exit(0)
+
     updater = Updater(TELEGRAM_TOKEN)
 
     updater.dispatcher.add_handler(CommandHandler('start', start))
-    updater.dispatcher.add_handler(MessageHandler(Filters.entity('url'), shot))
-    updater.dispatcher.add_handler(InlineQueryHandler(inline_shot))
+    updater.dispatcher.add_handler(
+        MessageHandler(Filters.entity('url'), answer)
+    )
+    updater.dispatcher.add_handler(InlineQueryHandler(inline_answer))
     updater.dispatcher.add_handler(
         MessageHandler(Filters.text & (~Filters.command), unknown)
     )
